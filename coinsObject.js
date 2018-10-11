008900f048d68d5c6c80bc9e42be5b6f63e547e6
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-10-11 23:37:31 +0000

    added coin object

---
+++ coinsObject.js
@@ -0,0 +1,14 @@
+var assignment = "create an object for coins";
+
+var currencyAbbrev = ["AUD", "GBP", "EUR", "JPY", "CHF", "USD", "AFN", "ALL", "DZD", "AOA"];
+
+var usd = {
+        halfDollar: .50,
+        quarter: .25,
+        dime: .10,
+        nickel: .05,
+        penny: .01
+}
+
+
+console.log("I have six " + currencyAbbrev[5]);
--- js_objects2.js
+++ js_objects2.js
@@ -4,7 +4,7 @@ var fruitTwo = {apples: 35,
 				
  				totalFruit: function() {
 					var allTheFruits = this.apples + this.oranges + this.grapes
-					return "There are " + allTheFruits + " total fruits in stock."
+					return "There are " + allTheFruits + " total fruit items in stock."
 				},
 				appleCount: function(adjustApple) {
 					var newAppleCount = this.apples + adjustApple
@@ -16,7 +16,7 @@ var fruitTwo = {apples: 35,
 				},
 				orangeCount: function(adjustOrange) {
 					var newOrangeCount = this.oranges + adjustOrange
-						if (newOrangeCount < 5) {
+						if (newOrangeCount < 10) {
 						return "There are " + newOrangeCount + " oranges in stock. We need to order more oranges ASAP!"
 					} else {
 						return "There are " + newOrangeCount + " oranges in stock. Sufficent oranges in stock, check orange inventory again in three days."
@@ -24,26 +24,25 @@ var fruitTwo = {apples: 35,
 				},
 				grapeCount: function(adjustGrape) {
 					var newGrapeCount = this.grapes + adjustGrape
-						if (newGrapeCount < 5) {
+						if (newGrapeCount < 50) {
 						return "There are " + newGrapeCount + " grapes in stock. We need to order more grapes ASAP!"
 					} else {
 						return "There are " + newGrapeCount + " grapes in stock. Sufficent grapes in stock, check grape inventory again in three days."
 					}
 				},
 				fruitSold: function(num) {
-					if (((this.apples + this.oranges + this.grapes) - num) < 20) {
-						return "There are now " + ((this.apples + this.oranges + this.grapes) - num) + " fruits in stock. We will run out of food to sell soon if order is not placed!!"
+					var soldFruit = (this.apples + this.oranges + this.grapes)
+					if ((soldFruit - num) < 30) {
+						return "There are now " + ((this.apples + this.oranges + this.grapes) - num) + " fruit items in stock. We will run out of food to sell soon if order is not placed!!"
 					} else {
-						return "There are now " + ((this.apples + this.oranges + this.grapes) - num) + " fruits in stock. Do complete inventory again in three days.";
+						return "There are now " + ((this.apples + this.oranges + this.grapes) - num) + " fruits items stock. Please complete inventory again in three days.";
 					}
 				}
 				
 			};
 
-
-
 console.log(fruitTwo.totalFruit());
 console.log(fruitTwo.appleCount(-31));
 console.log(fruitTwo.orangeCount(3));
 console.log(fruitTwo.grapeCount(-99));
-console.log(fruitTwo.fruitSold(100));
+console.log(fruitTwo.fruitSold(140));
